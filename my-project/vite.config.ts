a0dcfd0ae24de063a487f15bf3585822c87dd6f9
“chocapiklover” <a.harmuth33@gmail.com>
2024-05-15 14:16:29 +0200

    changed some configs for deployment

--- my-project/vite.config.ts
+++ my-project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react-router/',
+  base: 'https://chocapiklover.github.io/',
   
 })
  
