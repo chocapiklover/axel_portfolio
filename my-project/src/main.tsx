f22483d65defd5a354191ab023e1e22ae40a72bc
“chocapiklover” <a.harmuth33@gmail.com>
2024-05-15 14:58:21 +0200

    changed routers

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -3,29 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Home } from "./pages/Home.tsx";
-import { Contact } from "./pages/Contact.tsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/vite-react-router/",
-    element: <App />,
-    children: [
-      {
-        path: "/vite-react-router/",
-        element: <Home />,
-      },
-      {
-        path: "/vite-react-router/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
